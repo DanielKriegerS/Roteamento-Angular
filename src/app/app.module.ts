373cd8e3a1b933d490af40e078b99c36debff10f
DanielKriegerS <danielschifelbain@gmail.com>
2022-10-22 17:19:46 -0300

    Aula de Angular sobre roteamento. Com bug.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { PrimeiraPaginaComponent } from './primeira-pagina/primeira-pagina.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrimeiraPaginaComponent
   ],
   imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    BrowserModule,
+    AppRoutingModule
+  ]
 })
 export class AppModule { }
